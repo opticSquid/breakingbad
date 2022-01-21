fbd13d63626ee8d93d41455858e954a1f410a674
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-01-21 23:16:16 +0530

    troubleshoot drive 4

--- server/index.js
+++ server/index.js
@@ -6,21 +6,20 @@ const chalk = require("chalk");
 const redis = require("ioredis");
 //Uses port 6379 if not specified
 // other url is for using in heroku
-console.log(chalk.blue("Environment: ", process.env.node_env));
-if (process.env.node_env === "production") {
-  console.log(chalk.blue("Environment: ", process.env.node_env));
-  const client = new redis(process.env.REDIS_URL);
-  client.on("connect", () =>
-    console.log(chalk.green("Redis prod Client Connected"))
-  );
-  client.on("error", (err) => console.log("Redis prod Client Error", err));
-} else {
-  var client = redis.createClient(process.env.REDIS_URL);
-  client.on("connect", () =>
-    console.log(chalk.green("Redis dev Client Connected"))
-  );
-  client.on("error", (err) => console.log("Redis dev Client Error", err));
-}
+
+var client = redis.createClient(
+  process.env.REDIS_URL || {
+    host: "127.0.0.1",
+    port: 6379,
+    username: "",
+    password: "",
+  }
+);
+client.on("connect", () =>
+  console.log(chalk.green("Redis dev Client Connected"))
+);
+client.on("error", (err) => console.log("Redis dev Client Error", err));
+
 const port = process.env.PORT || 5000;
 const path = __dirname + "/build";
 app.use(cors());
